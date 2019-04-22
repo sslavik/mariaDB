6060b10db49c8c22e73666b7f35bf4f7c03a5d17
Slavik <sslavikshy@gmail.com>
2019-04-22 13:25:52 +0200

    ContarVictoriasSeguidas Corregido

--- Rutinas_SQL/ContarMaxVictorias.sql
+++ Rutinas_SQL/ContarMaxVictorias.sql
@@ -19,49 +19,44 @@ begin
     declare value1 int default 0;
     declare value2 int default 0;
     declare equipoL int;
-    declare equipoV int;
     declare contador int default 0;
     declare maxCont int default 0;
  
     -- CURSORS
-    declare res1 cursor for (select substring(resultado, 1, locate('-', resultado) - 1) from partido where (elocal = equipo or evisitante = equipo) and resultado is not null );
-    declare res2 cursor for (select substring(resultado, locate('-', resultado) + 1) from partido where (elocal = equipo or evisitante = equipo) and resultado is not null );
-    declare locl cursor for ( select elocal from partido );
-    declare visit cursor for ( select evisitante from partido );
-    declare exit handler for not found set final = true;
+    declare res1 cursor for (select substring(resultado, 1, locate('-', resultado) - 1) from partido where resultado is not null );
+    declare res2 cursor for (select substring(resultado, locate('-', resultado) + 1) from partido where resultado is not null );
+    declare CurEquipoL cursor for (select elocal from partido where resultado is not null );
+    declare continue handler for not found set final = true;
     
 
     open res1;
     open res2;
-    open locl;
-    open visit;
+    open CurEquipoL;
 
     while not final do 
         fetch res1 into value1;
         fetch res2 into value2;
-        fetch locl into equipoL;
-        fetch visit into equipoV;
+        fetch CurEquipoL into equipoL;
 
         set value1 = cast(value1 as int);
         set value2 = cast(value2 as int);
-        select value1, value2;
+
         if( value1 > value2 and equipoL = equipo ) then
             set contador = contador + 1;
-        elseif (value1 < value2 and equipoV = equipo ) then
-            set contador = contador + 1;
-        else
+        else 
             if (maxCont < contador) then
                 set maxCont = contador;
-                set contador = maxCont;
             end if;
             set contador = 0;
         end if;
+        
     end while;
+
+    set cantidad = maxCont;
 
     close res1;
     close res2;
-    close locl;
-    close visit;
+    close CurEquipoL;
 end//
 
 delimiter ;
